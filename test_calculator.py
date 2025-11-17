28997782e392b3b86d4448c0088dc464b56c5f10
MatiasChristensen <m.christensen1@ufl.edu>
2025-11-16 22:38:10 -0500

    modified testcalc mc

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator import *
+from calculator import add, sub, div, log
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -52,5 +52,33 @@ class TestCalculator(unittest.TestCase):
     ##########################
 
 # Do not touch this
+
+    def test_add():
+        assert add(2, 3) == 5
+        assert add(-1, 4) == 3
+        assert add(0, 0) == 0
+    
+    def test_subtract():
+        assert sub(10, 3) == 7
+        assert sub(4, 8) == -4
+        assert sub(0, 0) == 0
+    
+    def test_divide_by_zero():
+        with unittest.TestCase().assertRaises(ZeroDivisionError):
+            div(5, 0)
+        
+    def test_logarithm():
+        assert log(10, 100) == 2
+        assert log(2, 8) == 3
+
+    def test_log_invalid_base():
+        with unittest.TestCase().assertRaises(ValueError):
+            log(-2, 8)
+        with unittest.TestCase().assertRaises(ValueError):
+            log(0, 5) 
+        with unittest.TestCase().assertRaises(ValueError):
+            log(10, -3)
+
+
 if __name__ == "__main__":
     unittest.main()
